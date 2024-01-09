b101510a599ecbfa2c4bbf35f989ffe38e0fbacc
Anorak <93460138+AlexAnorak@users.noreply.github.com>
2024-01-09 19:51:03 +0800

    turn off cloudflare warp first

--- autorun.bat
+++ autorun.bat
@@ -1,7 +1,12 @@
+warp-cli disconnect
+warp-cli disconnect
+warp-cli disconnect
+ipgw login
 powershell -executionpolicy remotesigned -file "%appdata%\Microsoft\Windows\Start Menu\Programs\pondsihotspot.ps1"
-ipgw login
 TIMEOUT /T 60
 ipgw login
+powershell -executionpolicy remotesigned -file "%appdata%\Microsoft\Windows\Start Menu\Programs\pondsihotspot.ps1"
 TIMEOUT /T 60
 ipgw login
+powershell -executionpolicy remotesigned -file "%appdata%\Microsoft\Windows\Start Menu\Programs\pondsihotspot.ps1"
 exit
